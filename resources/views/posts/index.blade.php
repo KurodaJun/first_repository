7f04123ad6c35f2dbca9d40274511e4953b70c3e
KurodaJun <kuro.j.9610@gmail.com>
2023-06-22 17:46:06 +0900

    投稿作成画面の作成＆保存処理実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,6 +10,7 @@
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
+            <a href='/posts/create'>create</a>
             <div class='post'>
                 <h2 class='title'><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
                 <p class='body'>{{ $post->body }}</p>
